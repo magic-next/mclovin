9e7848fd0e65944d2b4a7199ff0ef0f4b6e9fe40
zerodois <felipe.rita@dcomp.sor.ufscar.br>
2019-08-20 13:05:01 -0300

    error middleware

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ require('dotenv').config();
 const Koa = require('koa');
 const logger = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
+
 const { createLogger } = require('./utils/logger');
 const Container = require('./utils/container');
 const controllers = require('./controllers');
 const services = require('./services');
 const models = require('./models');
 const routerFactory = require('./router');
+const errorMiddleware = require('./middlewares/error');
+
 const db = require('./db');
+const utils = require('./utils');
 const config = require('./config');
 
 const PORT = process.env.PORT || 3000;
@@ -21,6 +25,7 @@ container.register(services);
 container.register('router', routerFactory);
 container.register('db', db);
 container.register('config', config);
+container.register('utils', utils);
 const app = new Koa();
 
 app.use(logger());
@@ -29,6 +34,7 @@ app.use(bodyParser({ jsonLimit: '10mb' }));
 const { router } = container.draw;
 
 app
+  .use(errorMiddleware())
   .use(router.routes())
   .use(router.allowedMethods());
 
---
+++ src/middlewares/error.js
@@ -0,0 +1,19 @@
+const httpStatus = require('http-status');
+
+const factory = () => {
+  const middleware = async (ctx, next) => {
+    try {
+      await next();
+    } catch (error) {
+      ctx.status = error.status || httpStatus.INTERNAL_SERVER_ERROR;
+      ctx.body = {
+        error: true,
+        message: error.message,
+      };
+    }
+  };
+
+  return middleware;
+};
+
+module.exports = factory;
