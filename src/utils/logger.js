a4c0f459031424381d55ed065a956fdbb58e8b1c
zerodois <felipe.rita@dcomp.sor.ufscar.br>
2019-08-19 23:06:59 -0300

    logger

---
+++ src/utils/logger.js
@@ -0,0 +1,17 @@
+/**
+ * Create a application middleware logger
+ * @param {object} config
+ * @param {boolean} config.isDev
+ */
+const createLogger = ({ isDev = false }) => {
+  if (!isDev) {
+    // TODO: Logger in prod
+    const logger = { ...console };
+    logger.info = logger.log;
+    return logger;
+  }
+  const logger = { ...console };
+  logger.info = logger.log;
+  return logger;
+};
+exports.createLogger = createLogger;
